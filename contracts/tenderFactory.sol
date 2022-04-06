d911c6956ffbe2a0b632e46a24f997255b3a72c5
liyahshaikh <liyahshaikh965@gmail.com>
2022-04-07 03:54:35 +0530

    Tender Factory Contract Initialized and Bidding defined

---
+++ contracts/tenderFactory.sol
@@ -0,0 +1,24 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.4.16 <0.9.0;
+
+import "./Tender.sol";
+
+
+contract tenderFactory {
+    address[] allTenders;
+
+    function createTender(address tenderManager, string memory tenderTitle, string memory tenderId, string memory tenderDescription, 
+    uint bidSubmissionClosingDate, uint bidOpeningDate, uint covers, string[] memory clauses,
+    string[] memory taskName, uint[] memory taskDays, 
+    string[] memory constraints) public returns (address) {
+        Tender newTender = new Tender();
+        newTender.setTenderBasic(tenderManager, tenderTitle, tenderId, 
+        tenderDescription,
+        bidSubmissionClosingDate, bidOpeningDate, covers);
+        newTender.setTenderAdvanced(clauses,
+        taskName, taskDays, 
+        constraints);
+        allTenders.push(address(newTender));
+        return address(newTender);
+    }
+}
