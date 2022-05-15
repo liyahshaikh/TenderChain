ec1035c8977d3da15d2765b2fd9788eea4e5a911
Zoya-Arief <59862186+Zoya-Arief@users.noreply.github.com>
2022-05-15 14:34:06 +0530

    Update Tender2.sol

--- contracts/Tender2.sol
+++ contracts/Tender2.sol
@@ -1,4 +1,5 @@
 pragma solidity >=0.4.0;
+pragma experimental ABIEncoderV2;
 
 contract Tender{
 
@@ -6,6 +7,11 @@ contract Tender{
     uint public bidCount = 0;
     uint public contractorCount = 0;
     uint public bidderCount = 0;
+    address private winner;
+    uint private highestBid;
+
+    event tenderEndsWithWinner(address winner, uint bid);
+    event tenderEndsWithoutWinner();
 
     struct Contractor {
         uint id;
@@ -22,14 +28,15 @@ contract Tender{
         uint id;
         uint tenderId;
         string tenderTitle;
-        uint bid;
-        address userHash;
+        uint bidAmount;
+        address bidder;
         uint timeStamp;
+        BidStatus status;
     }
 
     struct Tender{
         uint id;
-        string tenderName;
+        string tenderName; 
         uint256 bidSubmissionClosingDate; 
         uint256 bidOpeningDate;
         uint256 bidRevelationDay;
@@ -52,7 +59,7 @@ contract Tender{
         Bidder,
         Contractor
     }
-
+    // maybe remove 
     enum ProposalStatus{
         verified,
         unverified,
@@ -62,6 +69,7 @@ contract Tender{
     enum BidStatus{
         processing,
         accepted,
+        negotiating,
         rejected   
     }
 
@@ -71,6 +79,8 @@ contract Tender{
     mapping (uint => address) public whoIsBidder;
     mapping (address => Contractor) public contractors;
     mapping (address => Bidder) public bidders;
+    mapping (address => bool) public bidExists;
+    mapping (uint => bool) public tenderExists; // see this aliya
 
     // contractor and bidder should be unique
     modifier alreadyPresent(address _address) {
@@ -87,11 +97,50 @@ contract Tender{
         }
         _;
     }
+    
+    
+    // fix needed for this modifier
+    modifier alreadyPresesntTender(uint _id){
+        require(!tenderExists[_id],
+        "Tender already exists");
+        _;
 
-    modifier alreadyPresesntTender()
+    }
+    // modifier to be wriiten
+    modifier onlyContractor(){
 
+    }
 
+    // modifier to be wriiten
+    modifier onlyBidder(){
 
+    }
+
+    modifier onlyBefore(uint time) {
+        require(now < time);
+        _;
+    }
+
+    modifier onlyAfter(uint time) {
+        require(now > time);
+        _;
+    }
+
+    function hasBidBefore(address bidder) public view returns (bool) {
+        ;
+    }
+    
+    function hasBeenChecked() public view onlyAfter(revelationEnd) returns (bool) {
+      return checkedByOwner;
+    }
+
+    // how to use? 
+    function generateHash(uint nonce, uint bidAmt) public pure returns (bytes32) {
+        bytes memory toHash = abi.encodePacked(nonce, bidAmt);
+        bytes32 hash = keccak256(toHash);
+        return hash;
+    }
+    
     function createContractor(string memory _username,) public alreadyPresent(msg.sender) {
         contractorCount++;
         whoIsContractor[contractorCount] = msg.sender;
@@ -103,8 +152,30 @@ contract Tender{
         whoIsBidder[bidderCount] = msg.sender;
         bidders[msg.sender] = Bidder(bidderCount, _username);
     }
+    
     // add functionality : no two tenders can be the same 
-    function createTender(string memory _tenderName, string memory _tenderDescription,uint256 _bidOpeningDate,  uint256 _bidSubmissionClosingDate, string _tenderDescription ,uint256 _finalTenderAmount) public {
-        tenderCount++;
-        tenders[tenderCount] = Tender(tenderCount,_tenderName , _tenderDescription, _bidOpeningDate, _bidSubmissionClosingDate, _tenderDescription, _finalTenderAmount, msg.sender);
+    // modifier to be added : only a contractor can create a tender 
+
+    function createTender(string memory _tenderName, string memory _tenderDescription,uint256 _bidOpeningDate,  uint256 _bidSubmissionClosingDate, string _tenderDescription ,uint256 _finalTenderAmount) public  {
+        if (tenderExists[_id]! = true){
+            tenderCount++;
+            tenders[tenderCount] = Tender(tenderCount, _tenderName , _tenderDescription, _bidOpeningDate, _bidSubmissionClosingDate, _tenderDescription, _finalTenderAmount, msg.sender);
+            tenderExists[_id]= true; // this line 
+        }
+        else{
+            require(tenderExists[_id] == true, "Tender already exists");
+        }   
     }
+
+    function createBid()
+
+
+
+
+
+
+
+
+
+
+}
